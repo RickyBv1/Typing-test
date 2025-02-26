ebc980f4e809289d319c16ef17928e779c1af7ac
RickyBv1 <rickycortes668668@gmail.com>
2025-02-26 08:12:41 -0500

    Refactor game structure: update script type, modify button IDs, enhance input handling, and improve visual feedback for gameplay

--- index.js
+++ index.js
@@ -1,102 +1,122 @@
-//HTML elements
-const beginButton = document.getElementById("begin");
-const progressBar = document.querySelector("#progressBar div");
-const correctElement = document.querySelector("#correctElement span");
-const input = document.querySelector("input");
-const incorrectElement = document.querySelector("#incorrectElement span");
-const ppmElement = document.querySelector("#ppmElement span");
-const end = document.querySelector("#end");
-const restartButton = document.querySelector("#end button");
+//Elements
+const words = wordsArray;
 const wordContainer = document.getElementById("currentWord");
+const previousWordContainer = document.getElementById("previousWord");
+const input = document.querySelector("input");
+const correctElement = document.querySelector("#correctElement span");
+const incorrectElement = document.querySelector("#incorrectElement span");
+const wpmElement = document.querySelector("#wpmElement span");
+const startButton = document.getElementById("start");
+const progressBar = document.querySelector("#progressBar div");
+const endContainer = document.getElementById("end");
 
 //Variables
-const gameTime = 60; // in seconds
+let lettersList;
+let currentIndex = 0;
 let correctLetters;
 let incorrectLetters;
-let finnishedWords;
-let lettersList = [];
-let currentIndex;
+let wordsFinished;
+let wpm;
 let playing = false;
-
-//functions
-function startGame() {
-    playing = true;
-    wordContainer.classList.toggle("hidden", false);
-    newWord();
-    let correctLetters = 0;
-    let incorrectLetters = 0;
-    let finnishedWords = 0;
-    end.classList.toggle("hidden", true);
-    lettersList[0].classList.toggle("currentLetter");
-    progressBar.classList.toggle("completeTime", true);
-    beginButton.classList.toggle("hidden", true);
-}
-
-function newWord() {
-    if(lettersList.length > 0) lettersList.forEach(letter => wordContainer.removeChild(letter));
-    const chosenWordNumber = Math.floor(Math.random() * wordsArray.length);
-    const chosenWord = wordsArray[chosenWordNumber];
-    lettersList = []
-    currentIndex = 0;
-    for (let i = 0; i < chosenWord.length; i++) {
-        const letterElement = document.createElement("span");
-        letterElement.textContent = chosenWord[i];
-        wordContainer.appendChild(letterElement);
-        lettersList.push(letterElement);
-    }
-}
-
-function createLetterEffect(element) {
-    element.classList.toggle("dissapear", true);
-    const letter = element.textContent
-    const letterPosition = element.getBoundingClientRect();
-    const newLetter = document.createElement("span");
-    newLetter.style = `
-    left: ${letterPosition.left}px;
-    top: ${letterPosition.top}px;
-    `
-    newLetter.classList.add("dissapear");
-    newLetter.textContent = letter;
-    document.body.appendChild(newLetter);
-}
-
-//Events
-beginButton.addEventListener("click", () => startGame());
-restartButton.addEventListener("click", () => startGame());
-
-progressBar.addEventListener("animationend", () => {
-    playing = false;
-    end.classList.toggle("hidden", false);
-    progressBar.classList.toggle("completeTime", false);
-    correctElement.textContent = correctLetters;
-    incorrectElement.textContent = incorrectLetters;
-    ppmElement.textContent = finnishedWords * (60 / gameTime);
-    wordContainer.classList.toggle("hidden", true);
-})
-
-
-//Execution
-input.focus();
+const gameTime = 60; //in seconds
 document.documentElement.style.setProperty("--gameTime", gameTime + "s");
-//newWord();
-input.addEventListener("input", (event) => {
-    if(!playing) {
-        if(event.data === " ") startGame();
-        return;
-    }
 
-    if(event.data === lettersList[currentIndex].textContent) {
-        createLetterEffect(lettersList[currentIndex]);
-        currentIndex++;
-        correctLetters++;
-        if(currentIndex === lettersList.length) {
-            newWord();
-            finnishedWords++;
-        }
-        lettersList[currentIndex].classList.toggle("currentLetter");
-    } else {
-        incorrectElement++;
-    }
-    
+//Functions
+function newWord() {
+	currentIndex = 0;
+	const chosenWordNumber = Math.floor(Math.random() * (words.length - 1));
+	const chosenWord = words[chosenWordNumber];
+	deleteChildren(wordContainer);
+	lettersList = [];
+	for (let i = 0; i < chosenWord.length; i++) {
+		const letterElement = document.createElement("span");
+		letterElement.textContent = chosenWord[i];
+		letterElement.classList.add("show");
+		wordContainer.appendChild(letterElement);
+		lettersList.push(letterElement);
+		letterElement.addEventListener("animationend", ()=>{
+			letterElement.classList.toggle("show",false);
+		})
+	}
+}
+
+function startGame(){
+	playing = true;
+	startButton.classList.toggle("hidden",true);
+	wordContainer.classList.toggle("hidden",false);
+	resetGame();
+	newWord();
+	progressBar.classList.toggle("completeTime",true);
+	endContainer.classList.toggle("hidden",true);
+	lettersList[0].classList.add("currentLetter")
+}
+
+function finishGame(){
+	playing = false;
+	wordContainer.classList.toggle("hidden",true);
+	endContainer.classList.toggle("hidden",false);
+	progressBar.classList.toggle("completeTime",false);
+	wpm = wordsFinished * (60 / gameTime);
+	wpmElement.textContent = wpm;
+}
+
+function resetGame(){
+	correctLetters = 0;
+	incorrectLetters = 0;
+	wpm = 0;
+	wordsFinished = 0;
+	correctElement.textContent = correctLetters;
+	incorrectElement.textContent = incorrectLetters;
+}
+
+function deleteChildren(element){
+	Array.from(element.children).forEach(child => element.removeChild(child))
+}
+
+function createLetterEffect(element){
+	const letter = element.textContent;
+	const letterPosition = element.getBoundingClientRect();
+	element.classList.add("invisible");
+	const newLetter = document.createElement("span");
+	newLetter.textContent=letter;
+	newLetter.style = `
+		--top: ${letterPosition.top}px;
+		--left: ${letterPosition.left}px;
+	`
+	newLetter.classList.add("disappear");
+	document.body.appendChild(newLetter);
+	newLetter.addEventListener("animationend", ()=>{
+		document.body.removeChild(newLetter);
+	})
+}
+
+// Events
+progressBar.addEventListener("animationend", ()=>{
+	finishGame();
+})
+document.getElementById("start").addEventListener("click", ()=> startGame());
+input.addEventListener("input",(event)=>{
+	if(!playing) {
+		if(event.data === " ") startGame();
+		return;
+	} 
+	if(lettersList[currentIndex].textContent === event.data){
+		createLetterEffect(lettersList[currentIndex])
+		currentIndex++;
+		correctLetters++
+		correctElement.textContent = correctLetters;
+		if(currentIndex === lettersList.length) {
+			newWord()
+			wordsFinished++;
+		};
+		lettersList[currentIndex].classList.add("currentLetter")
+	}
+	else {
+		incorrectLetters++
+		incorrectElement.textContent = incorrectLetters;
+	}
 });
-input.addEventListener("blur", () => input.focus());
+input.addEventListener("blur",()=> input.focus())
+
+//Execute
+input.focus();
